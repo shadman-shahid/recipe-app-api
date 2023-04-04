cefa40b040188100e4d81baee8b00f676a1b0f92
shadman-shahid <shadman9085@gmail.com>
2023-04-04 10:38:06 +0600

    Added postgresql capabilities.

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator Function
+"""
+
+
+def add(x, y):
+    return x+y
+
+def subtract(x, y):
+    return y-x
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module."""
+    def test_add_numbers(self):
+        """ test the calc module."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting number"""
+
+        res = calc.subtract(6,5)
+        self.assertEqual(res, 1)
