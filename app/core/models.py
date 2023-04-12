e205f4e3568e66f73c40c0f03e8b2164124a3e1a
shadman-shahid <shadman9085@gmail.com>
2023-04-12 08:50:41 +0600

    Created user models and made migrations.

--- app/core/models.py
+++ app/core/models.py
@@ -35,7 +35,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-
+    objects = UserManager()
     # Replace the username field
     USERNAME_FIELD = 'email'
 
